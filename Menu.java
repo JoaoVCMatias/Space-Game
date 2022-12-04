4eeaa4b617015d1a2e20a3dbb44eed9dccd9e078
Pedro Gobira <pedrogobira@tutanota.com>
2022-12-03 21:47:03 -0300

    refactor: remove comentário

--- Menu.java
+++ Menu.java
@@ -16,7 +16,6 @@ public class Menu extends World
      */
     public Menu()
     {    
-        // Cria novo mundo de resolucao 600x400 com celula de 1 pixel
         super(800, 600, 1);
         addObject(new Play(new MyWorld(Level.generateLevel1())), 350, 140);
         addObject(new Quit(new MyWorld(Level.generateLevel1())), 350, 448);
